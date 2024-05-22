a85b703ec0cbbde15433824382a088403b6ee5ad
Yin Zhang <14476+melcloud@users.noreply.github.com>
2024-05-22 21:08:53 +1000

    Change ssh key due to reset

--- nixosModules/user_nixos.nix
+++ nixosModules/user_nixos.nix
@@ -2,7 +2,7 @@
 { config, lib, pkgs, ... }: {
   users.users.nixos = {
     openssh.authorizedKeys.keys = [
-      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIFq1f00Z7ifUTw0AbeEwzWB3C+dbAofYRx1bz4eZVyZMAAAAE3NzaDpuaXhvcy1pbnN0YWxsZXI="
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAILwGInIGXAhrdBhLSfU4OigLt0bhKS7lJjh48nD/zOpfAAAAE3NzaDpuaXhvcy1pbnN0YWxsZXI= melcloud@frame-arch"
     ];
     extraGroups = [
       "ssh-user"
